ebc056a71104bb57acc55abe4f0b08759703da37
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2014-10-06 11:16:06 -0400

    fixed build method naming.

--- lib/perf/Db/QueryFilter.php
+++ lib/perf/Db/QueryFilter.php
@@ -29,7 +29,7 @@ class QueryFilter
      *
      * @return QueryFilterBuilder
      */
-    public static function getBuilder()
+    public static function createBuilder()
     {
         return new QueryFilterBuilder();
     }
