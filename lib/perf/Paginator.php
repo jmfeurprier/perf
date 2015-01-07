aa426f2318d9c80ca055bf7218240f59af3b7abf
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2015-01-06 23:51:32 -0500

    paginator cleanup

--- lib/perf/Paginator.php
+++ lib/perf/Paginator.php
@@ -53,6 +53,20 @@ class Paginator
     private $lastPage;
 
     /**
+     * Index of the previous page, if any.
+     *
+     * @var null|int
+     */
+    private $previousPage;
+
+    /**
+     * Index of the next page, if any.
+     *
+     * @var null|int
+     */
+    private $nextPage;
+
+    /**
      * Constructor.
      *
      * @param int $itemCount Total number of items to paginate.
@@ -60,18 +74,41 @@ class Paginator
      * @param int $currentPage Index of the current page.
      * @param int $firstPage Index of the first page.
      * @return void
+     * @throws \InvalidArgumentException
      */
     public function __construct($itemCount, $itemsPerPage, $currentPage, $firstPage = 1)
     {
-        $this->itemCount    = max(0, (int) $itemCount);
-        $this->itemsPerPage = max(1, (int) $itemsPerPage);
-
+        $this->validateGreaterOrEquals('item count', 0, $itemCount);
+        $this->validateGreaterOrEquals('items per page', 1, $itemsPerPage);
+        $this->validateGreaterOrEquals('first page', 0, $firstPage);
+        
+        $this->itemCount    = $itemCount;
+        $this->itemsPerPage = $itemsPerPage;
         $this->pageCount    = max(1, (int) ceil($this->itemCount / $this->itemsPerPage));
-
-        $this->firstPage    = (int) $firstPage;
+        $this->firstPage    = $firstPage;
         $this->lastPage     = ($this->firstPage + $this->pageCount - 1);
 
         $this->setCurrentPage($currentPage);
+    }
+
+    /**
+     *
+     *
+     * @param string $parameter
+     * @param int $threshold
+     * @param int $value
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    private function validateGreaterOrEquals($parameter, $threshold, $value)
+    {
+        $this->validateInteger($parameter, $value);
+
+        if ($value < $threshold) {
+            $message = "Provided {$parameter} is not greater or equal to {$value}.";
+
+            throw new \InvalidArgumentException($message);
+        }
     }
 
     /**
@@ -82,9 +119,41 @@ class Paginator
      */
     public function setCurrentPage($currentPage)
     {
-        $this->currentPage = min($this->lastPage, max($this->firstPage, (int) $currentPage));
+        $this->validateInteger('current page', $currentPage);
+
+        // Fix current page index if needed.
+        $this->currentPage = min($this->lastPage, max($this->firstPage, $currentPage));
+
+        if ($this->currentPage > $this->firstPage) {
+            $this->previousPage = $this->currentPage - 1;
+        } else {
+            $this->previousPage = null;
+        }
+
+        if ($this->currentPage < $this->lastPage) {
+            $this->nextPage = $this->currentPage + 1;
+        } else {
+            $this->nextPage = null;
+        }
 
         return $this;
+    }
+
+    /**
+     *
+     *
+     * @param string $parameter
+     * @param int $value
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    private function validateInteger($parameter, $value)
+    {
+        if (!is_int($value)) {
+            $message = "Provided {$parameter} is not an integer.";
+
+            throw new \InvalidArgumentException($message);
+        }
     }
 
     /**
@@ -138,14 +207,27 @@ class Paginator
     }
 
     /**
-     * Returns the index of the previous page (may be equal to the current page index, if current page
-     *   is the first page).
+     * Returns the index of the previous page.
      *
      * @return int
      */
     public function getPreviousPage()
     {
-        return max($this->firstPage, ($this->currentPage - 1));
+        if ($this->hasPreviousPage()) {
+            return $this->previousPage;
+        }
+
+        throw new \RuntimeException();
+    }
+
+    /**
+     * Returns true if a previous page exists (ie, if current page is not the first page).
+     *
+     * @return bool
+     */
+    public function hasPreviousPage()
+    {
+        return !is_null($this->previousPage);
     }
 
     /**
@@ -155,7 +237,21 @@ class Paginator
      */
     public function getNextPage()
     {
-        return min($this->lastPage, ($this->currentPage + 1));
+        if ($this->hasNextPage()) {
+            return $this->nextPage;
+        }
+
+        throw new \RuntimeException();
+    }
+
+    /**
+     * Returns true if a next page exists (ie, if current page is not the last page).
+     *
+     * @return bool
+     */
+    public function hasNextPage()
+    {
+        return !is_null($this->nextPage);
     }
 
     /**
